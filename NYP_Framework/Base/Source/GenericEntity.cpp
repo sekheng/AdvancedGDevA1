90f553d34c3d28d2251e5686c991c03bebc750ea
Ah Heng <leesekheng@yahoo.com>
2016-12-11 17:07:21 +0800

    changing the entity manager again.

--- NYP_Framework/Base/Source/GenericEntity.cpp
+++ NYP_Framework/Base/Source/GenericEntity.cpp
@@ -68,7 +68,7 @@ GenericEntity* Create::Entity(	const std::string& _meshName,
 	result->SetPosition(_position);
 	result->SetScale(_scale);
 	result->SetCollider(false);
-	EntityManager::GetInstance()->AddEntity(result);
+	//EntityManager::GetInstance()->AddEntity(result);
 	return result;
 }
 
